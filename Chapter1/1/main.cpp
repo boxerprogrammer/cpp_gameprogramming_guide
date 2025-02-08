ebb199a02acca13f318a3f2309faa338deeeb06d
Ryuichi Kawano <zan56011@nifty.com>
2025-02-09 01:27:46 +0900

    ジャンプまで

--- Chapter1/1/main.cpp
+++ Chapter1/1/main.cpp
@@ -8,12 +8,12 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	//âÊëúÇÃÉçÅ[Éh
-	int playerH = LoadGraph(L"img/Abeja.png");
+	int playerH = LoadGraph(L"../img/Abeja.png");
 
 	while(ProcessMessage()!=-1){
 		ClearDrawScreen();
 		DrawBox(0, 0, 640, 480, GetColor(64, 128, 255), true);
-		DrawRectRotaGraph(320, 240,0,0,240,240,0.5f,0.0f, playerH, true);
+		DrawRectRotaGraph(320, 240,0,0,240,240,0.3f,0.0f, playerH, true);
 		ScreenFlip();
 	}
 	return 0;
--- Chapter1/2/main.cpp
+++ Chapter1/2/main.cpp
@@ -1,4 +1,5 @@
 #include<DxLib.h>
+#include<cassert>
 
 int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
 	ChangeWindowMode(true);
@@ -8,15 +9,36 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	//画像のロード
-	int playerH = LoadGraph(L"img/Abeja.png");
+	int playerH = LoadGraph(L"../img/Abeja.png");
 
-	float vy = 0.0f;
-	float g = 0.1f;
+	float y = 240.0f;//自機のY座標
+	float vy = 0.0f;//自機のY方向速度
+	constexpr float g = 0.3f;//重力加速度
+	constexpr float jump_velocity = -6.0f;//ジャンプの瞬間速度(上向き)
+
+	//ゲームスピードを一定に保つための工夫
+	auto lastTime = GetTickCount64();//直前のミリ秒を計っておく
+	constexpr auto flip_interval = 16U;//画面更新当たりのミリ秒(1000/60=16.66だが16ミリ秒とする)
+
+	auto lastMouseSate = GetMouseInput();
 
 	while(ProcessMessage()!=-1){
 		ClearDrawScreen();
+
+		//クリックされたら空中ジャンプする(ぴょん)
+		auto currentMouseState = GetMouseInput();
+		if(!(lastMouseSate&MOUSE_INPUT_LEFT) && (currentMouseState&&MOUSE_INPUT_LEFT)){
+			vy = jump_velocity;
+		}
+
 		DrawBox(0, 0, 640, 480, GetColor(64, 128, 255), true);
-		DrawRectRotaGraph(320, 240,0,0,240,240,0.5f,0.0f, playerH, true);
+		DrawRectRotaGraphF(320.0f, y,0,0,240,240,0.3f,0.0f, playerH, true);
+		vy += g;
+		y += vy;
+		auto waitTime=GetTickCount64() - lastTime;
+		if(waitTime<flip_interval){//16ミリ秒経ってなかったら16ミリ秒になるまで待つ
+			Sleep((unsigned int)waitTime);
+		}
 		ScreenFlip();
 	}
 	return 0;
