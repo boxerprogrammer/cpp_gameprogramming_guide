72312d23b4d6b5cbab6172ee70195eb506e95f06
Ryuichi Kawano <zan56011@nifty.com>
2025-04-10 09:46:41 +0900

    重力を追加、クリックではねるように

--- Chapter1/2/main.cpp
+++ Chapter1/2/main.cpp
@@ -1,6 +1,6 @@
 #include<DxLib.h>
 #include<cassert>
-
+#include<cmath>
 int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
 	ChangeWindowMode(true);
 	if(DxLib_Init()==-1){
@@ -9,10 +9,11 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	//画像のロード
-	int playerH = LoadGraph(L"../img/Abeja.png");
+	int playerH = LoadGraph(L"../img/bird.png");
 
 	float y = 240.0f;//自機のY座標
 	float vy = 0.0f;//自機のY方向速度
+	float vx = 3.0f;//自機のX方向速度
 	constexpr float g = 0.3f;//重力加速度
 	constexpr float jump_velocity = -6.0f;//ジャンプの瞬間速度(上向き)
 
@@ -21,20 +22,23 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
 	constexpr auto flip_interval = 16U;//画面更新当たりのミリ秒(1000/60=16.66だが16ミリ秒とする)
 
 	auto lastMouseSate = GetMouseInput();
-
+	
 	while(ProcessMessage()!=-1){
 		ClearDrawScreen();
 
-		//クリックされたら空中ジャンプする(ぴょん)
+		//クリックされたら空中ジャンプする
 		auto currentMouseState = GetMouseInput();
 		if(!(lastMouseSate&MOUSE_INPUT_LEFT) && (currentMouseState&&MOUSE_INPUT_LEFT)){
 			vy = jump_velocity;
 		}
-
-		DrawBox(0, 0, 640, 480, GetColor(64, 128, 255), true);
-		DrawRectRotaGraphF(320.0f, y,0,0,240,240,0.3f,0.0f, playerH, true);
 		vy += g;
 		y += vy;
+
+
+		DrawBox(0, 0, 640, 480, GetColor(64, 128, 255), true);
+		float angle = atan2f(vy, vx);//速度ベクトルを角度に変換
+		DrawRectRotaGraphF(320.0f, y,0,0,16,16,3.0f,angle, playerH, true);
+
 		auto waitTime=GetTickCount64() - lastTime;
 		if(waitTime<flip_interval){//16ミリ秒経ってなかったら16ミリ秒になるまで待つ
 			Sleep((unsigned int)waitTime);
